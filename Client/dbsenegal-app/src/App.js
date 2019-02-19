e1b0500f2ffcf5561805e22ce9a38026a0ea0b31
LinusKl <linus.kliss94@gmail.com>
2019-02-19 17:35:05 +0100

    added crossorigin support to fetch data from within webapp

--- Client/dbsenegal-app/src/App.js
+++ Client/dbsenegal-app/src/App.js
@@ -3,26 +3,68 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      patients: null,
+      isLoading: true
+    }
+  }
+
+  componentDidMount(){
+    this.setState({isLoading: true});
+
+    fetch('http://localhost:8080/patient-list')
+    .then(response => response.json())
+    .then(data => this.setState({patients: data, isLoading: false}));
+  }
+
   render() {
+
+    const{patients, isLoading} = this.state;
+
+    if(isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>DB Senegal</p>
         </header>
+        <div>
+
+        <table>
+          <tr className="headline">
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Address</td>
+            <td>Birthday</td>
+          </tr>
+          {patients.map((patient: any) =>
+            <tr key={patient.id}>
+              <td>{patient.firstName}</td>
+              <td>{patient.familyName}</td>
+              <td>{patient.address}</td>
+              <td>{patient.birthday}</td>
+            </tr>
+          )}
+        </table>
+        </div>
+
       </div>
     );
   }
 }
+
+
+
+
+
+
+
+
 
 export default App;
