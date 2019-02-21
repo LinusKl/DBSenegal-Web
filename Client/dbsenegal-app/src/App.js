dbe07dafd2b6ee93ab6992cad1c5a0b47d27ecd6
LinusKl <linus.kliss94@gmail.com>
2019-02-21 15:22:03 +0100

    Added Routing

--- Client/dbsenegal-app/src/App.js
+++ Client/dbsenegal-app/src/App.js
@@ -5,57 +5,10 @@ import AddPatient from './components/AddPatient.js';
 import LoginPage from './components/LoginPage.js';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-
-const Home = (props) => {
-  return(
-    <div>Home</div>
-  )
-}
-const Settings = (props) => {
-  return(
-    <div>Settings</div>
-  )
-}
-const About = (props) => {
-  return(
-    <div>About</div>
-  )
-}
-
-
-
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      patients: null,
-      isLoading: true
-    }
-  }
-
-  componentDidMount(){
-    this.setState({isLoading: true});
-
-    fetch('http://localhost:8080/patient-list')
-    .then(response => response.json())
-    .then(data => this.setState({patients: data, isLoading: false}));
-  }
 
   render() {
-
-    const{patients, isLoading} = this.state;
-
-    if(isLoading) {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <p>Loading...</p>
-          </header>
-        </div>
-      );
-    }
 
     return (
       <div className="App">
@@ -69,7 +22,7 @@ class App extends Component {
             </header>
             <div>
                 <div>
-                  <Route exact path='/' compoent={LoginPage} />
+                  <Route exact path='/' component={LoginPage} />
                   <Route exact path='/patients-list' component={PatientsList}/>
                   <Route exact path='/patient-modify' component={AddPatient} />
                 </div>
--- Client/dbsenegal-app/src/components/PatientsList.js
+++ Client/dbsenegal-app/src/components/PatientsList.js
@@ -3,16 +3,40 @@ import React, {Component} from 'react';
 
 class PatientsList extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      patients: null,
+      isLoading: true
+    }
+
+  }
+
+
+  componentDidMount(){
+    this.setState({isLoading: true});
+
+    fetch('http://localhost:8080/patient-list')
+    .then(response => response.json())
+    .then(data => this.setState({patients: data, isLoading: false}));
+
+    console.log(this.patients);
+  }
+
   render(){
-    {console.log(this.props.patients)}
-    return patientList(this.props);
+
+    const{patients, isLoading} = this.state;
+
+    if(this.state.isLoading){
+      return (<h3> Loading... </h3>);
+    }
+    return patientList(patients);
   }
 
 }
 
-
-function patientList(props) {
-
+function patientList(patients) {
   return(
     <table>
       <tbody>
@@ -23,7 +47,7 @@ function patientList(props) {
           <td>Birthday</td>
         </tr>
 
-        {props.patients.map((patient) =>
+        {patients.map((patient) =>
         <tr key={patient.id}>
           <td>{patient.firstName}</td>
           <td>{patient.familyName}</td>
@@ -37,26 +61,4 @@ function patientList(props) {
   );
 }
 
-function patientListShort(props) {
-
-  return(
-    <table>
-      <tbody>
-        <tr className="headline">
-          <td>First Name</td>
-          <td>Last Name</td>
-        </tr>
-
-        {props.patients.map((patient) =>
-        <tr key={patient.id}>
-          <td>{patient.firstName}</td>
-          <td>{patient.familyName}</td>
-        </tr>
-        )}
-
-      </tbody>
-    </table>
-  );
-}
-//<PatientsList patients={patients}/>
 export default PatientsList;
